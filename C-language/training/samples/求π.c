a8dff02bbdc1c1a0ef8d307bd35d2a38e5c55684
vjudge <gradonday@gmail.com>
2021-08-05 22:14:20 +0800

    add pai

---
+++ C-language/training/samples/求π.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// 求π
+int main(int argc, char *argv[]) {
+	int n = 0, m = 0; 
+	for (int i = 0; i < 1000000; i++) { 
+		double x = 1.0 * rand() / RAND_MAX; 
+		double y = 1.0 * rand() / RAND_MAX; 
+		if (x * x + y * y <= 1.0) {
+			m += 1; 
+		}
+		n += 1; 
+	} 
+	printf("%lf\n", 4.0 * m / n); 
+	return 0;
+}
