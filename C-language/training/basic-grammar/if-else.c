8a21e430d4af44aece909dd0334c42e238c596ae
vjudge <gradonday@gmail.com>
2021-08-05 22:13:42 +0800

    add if-else

---
+++ C-language/training/basic-grammar/if-else.c
@@ -0,0 +1,21 @@
+# include "stdio.h"
+
+// 判断输入的两个数是否相等
+int main () {
+	int a = 0;
+	int b = 0;
+	scanf("%d,%d", &a, &b);
+	if (a == b) {
+		printf("a is equal to b\n");
+	} else {
+		printf("a is not equal to b\n");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
